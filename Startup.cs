5c92a9d58f09cb93abb22e27673e4e13dff43bd2
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-12-27 02:39:50 -0300

    Added: AutoMapper service to Startup

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using dotnet_rpg.Services.CharacterService;
+using AutoMapper;
 
 namespace dotnet_rpg
 {
@@ -33,6 +34,7 @@ namespace dotnet_rpg
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnet_rpg", Version = "v1" });
             });
+            services.AddAutoMapper(typeof(Startup));
             services.AddScoped<ICharacterService, CharacterService>();
         }
 
