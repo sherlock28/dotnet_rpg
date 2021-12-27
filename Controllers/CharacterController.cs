df844e2bc606d8f42f18bd42934d4a8f53385d96
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-12-27 02:45:46 -0300

    Change: Used 'GetCharacterDto' and 'AddCharacterDto' in 'CharacterService', 'ICharacterService' and 'CharacterController'. Added: Auto generate consecutive Ids

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -3,6 +3,7 @@ using dotnet_rpg.Models;
 using System.Collections.Generic;
 using dotnet_rpg.Services.CharacterService;
 using System.Threading.Tasks;
+using dotnet_rpg.Dtos.Character;
 
 namespace dotnet_rpg.Controllers
 {
@@ -18,19 +19,19 @@ namespace dotnet_rpg.Controllers
         }
 
         [HttpGet("GetAll")]
-        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
         {
             return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id)
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingle(int id)
         {
             return Ok(await _characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter)
         {
             return Ok(await _characterService.AddCharacter(newCharacter));
         }
