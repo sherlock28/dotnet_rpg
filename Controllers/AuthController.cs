aa9d2854533036b23e26b36f914f0b1ff51b9a6c
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-12-30 03:59:39 -0300

    Added: AuthController with Register method implemented

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using dotnet_rpg.Data;
+using dotnet_rpg.Dtos.User;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers 
+{ 
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+
+        public AuthController(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _authRepository.Register(
+                new User { Username = request.Username }, request.Password
+            );
+
+            if(!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+        
+    }
+}
