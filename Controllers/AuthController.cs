2a2ec1db98181b8bd753ec11166f34e2cd73ae87
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-12-30 04:43:03 -0300

    Added: Login method implementation to AuthController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -4,8 +4,8 @@ using dotnet_rpg.Dtos.User;
 using dotnet_rpg.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace dotnet_rpg.Controllers 
-{ 
+namespace dotnet_rpg.Controllers
+{
     [ApiController]
     [Route("[controller]")]
     public class AuthController : ControllerBase
@@ -24,13 +24,25 @@ namespace dotnet_rpg.Controllers
                 new User { Username = request.Username }, request.Password
             );
 
-            if(!response.Success)
+            if (!response.Success)
             {
                 return BadRequest(response);
             }
 
             return Ok(response);
         }
-        
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<string>>> Login(UserLoginDto request)
+        {
+            var response = await _authRepository.Login(request.Username, request.Password);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
     }
 }
