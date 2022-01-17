8259c3f1a84863fba863488083bbc3e601cabbad
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-01-17 18:45:02 -0300

    Added: Implementation of the WeaponService

---
+++ Services/WeaponService/WeaponService.cs
@@ -0,0 +1,64 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using dotnet_rpg.Data;
+using dotnet_rpg.Dtos.Character;
+using dotnet_rpg.Dtos.Weapon;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Services.WeaponService
+{
+    public class WeaponService : IWeaponService
+    {
+
+        private readonly DataContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IMapper _mapper;
+
+        public WeaponService(DataContext context, IHttpContextAccessor httpContextAccessor, IMapper mapper)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+            _mapper = mapper;
+        }
+
+        public async Task<ServiceResponse<GetCharacterDto>> AddWeapon(AddWeaponDto newWeapon)
+        {
+            var serviceResponse = new ServiceResponse<GetCharacterDto>();
+
+            try
+            {
+
+                var character = await _context.Characters
+                    .FirstOrDefaultAsync(c => c.Id == newWeapon.CharacterId
+                        && c.User.Id == int.Parse(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)));
+                if (character == null)
+                {
+                    serviceResponse.Success = false;
+                    serviceResponse.Message = "Character not found.";
+                    return serviceResponse;
+
+                }
+                var weapon = new Weapon
+                {
+                    Name = newWeapon.Name,
+                    Damage = newWeapon.Damage,
+                    Character = character
+                };
+
+                _context.Weapons.Add(weapon);
+                await _context.SaveChangesAsync();
+                
+                serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+            }
+            catch (System.Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+            }
+            return serviceResponse;
+        }
+    }
+}
