b2b818af66474558ec40954d1a6eda958fac69e5
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-01-18 15:44:28 -0300

    Added: FightController with the endpoint /fight/weapon/attack

---
+++ Controllers/FightController.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using dotnet_rpg.Dtos.Fight;
+using dotnet_rpg.Models;
+using dotnet_rpg.Services.FightService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FightController : ControllerBase
+    {
+        private readonly IFightService _fightService;
+
+        public FightController(IFightService fightService)
+        {
+            _fightService = fightService;
+        }
+
+        [HttpPost("Weapon/Attack")]
+        public async Task<ActionResult<ServiceResponse<AttackResultDto>>> WeaponAttack(WeaponAttackDto request)
+        {
+            return Ok(await _fightService.WeaponAttack(request));
+        }
+    }
+}
