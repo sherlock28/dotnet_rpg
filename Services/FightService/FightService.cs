be10a6ef1f27fd49e07817a37d4aeeb8dcb9020a
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-01-19 09:37:38 -0300

    Added: SkillAttack method to IFightService interface and implementation of the service to FightService

--- Services/FightService/FightService.cs
+++ Services/FightService/FightService.cs
@@ -74,5 +74,71 @@ namespace dotnet_rpg.Services.FightService
             }
             return serviceResponse;
         }
+    
+        public async Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request)
+        {
+            var serviceResponse = new ServiceResponse<AttackResultDto>();
+            try
+            {
+                var attacker = await _context.Characters
+                .Include(c => c.Skills)
+                .FirstOrDefaultAsync(c => c.Id == request.AttackerId);
+                if (attacker == null)
+                {
+                    serviceResponse.Success = false;
+                    serviceResponse.Message = "Character not found.";
+                    return serviceResponse;
+                }
+                var opponent = _context.Characters.FirstOrDefault(c => c.Id == request.OpponentId);
+                if (opponent == null)
+                {
+                    serviceResponse.Success = false;
+                    serviceResponse.Message = "Character not found.";
+                    return serviceResponse;
+                }
+
+                var skill = attacker.Skills.FirstOrDefault(s => s.Id == request.SkillId);
+                if(skill == null) 
+                {
+                    serviceResponse.Success = false;
+                    serviceResponse.Message = $"{attacker.Name} does not have this skill.";
+                    return serviceResponse;
+                } 
+
+                int damage = skill.Damage + (new Random().Next(attacker.Intelligence) / 2);
+                damage -= (new Random().Next(opponent.Defence) / 2);
+
+                if (damage > 0)
+                {
+                    opponent.HitPoints -= damage;
+                }
+
+                if(opponent.HitPoints <= 0)
+                {
+                    opponent.HitPoints = 0;
+                    attacker.Victories++;
+                    opponent.Defeats++;
+                    serviceResponse.Success = false;
+                    serviceResponse.Message = $"{opponent.Name} has been defeated!";
+                }
+
+                await _context.SaveChangesAsync();
+
+                serviceResponse.Data = new AttackResultDto
+                {
+                    Attacker = attacker.Name,
+                    AttackerHP = attacker.HitPoints,
+                    Opponent = opponent.Name,
+                    OpponentHP = opponent.HitPoints,
+                    Damage = damage
+                };      
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+            }
+            return serviceResponse;
+        }
     }
 }
