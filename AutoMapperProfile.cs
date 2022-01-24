bd42ece6302d7325fbdc45638e66a894150c6310
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-01-23 23:43:15 -0300

    Change: Added HighscoreDto model to AutoMapperProfile

--- AutoMapperProfile.cs
+++ AutoMapperProfile.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using dotnet_rpg.Dtos.Character;
 using dotnet_rpg.Dtos.Weapon;
 using dotnet_rpg.Dtos.Skill;
+using dotnet_rpg.Dtos.Fight;
 using dotnet_rpg.Models;
 
 namespace dotnet_rpg.Services.CharacterService
@@ -14,6 +15,7 @@ namespace dotnet_rpg.Services.CharacterService
             CreateMap<AddCharacterDto, Character>();
             CreateMap<Weapon, GetWeaponDto>();
             CreateMap<Skill, GetSkillDto>();
+            CreateMap<Character, HighscoreDto>();
         }
     }
 }
